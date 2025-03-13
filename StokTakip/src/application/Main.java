67a8a16359aa432b52bde33628237ceb49014347
DOĞUKAN DİNÇ <32507641+dogukandinc99@users.noreply.github.com>
2025-03-14 01:05:45 +0300

    update

--- StokTakip/src/application/Main.java
+++ StokTakip/src/application/Main.java
@@ -9,8 +9,25 @@ import javafx.scene.Scene;
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
+
 		try {
-			DataBaseHelper.createTable();
+
+			DataBaseHelper.createTable("kategoriler", """
+						    CREATE TABLE IF NOT EXISTS kategoriler (
+					        id INTEGER PRIMARY KEY AUTOINCREMENT,
+					        ad TEXT NOT NULL
+					    );
+					""");
+			DataBaseHelper.createTable("stok", """
+						    CREATE TABLE IF NOT EXISTS stok (
+					        id INTEGER PRIMARY KEY AUTOINCREMENT,
+					        barkod TEXT UNIQUE NOT NULL,
+					 		urun_adi TEXT NOT NULL,
+					 		urun_adet INTEGER NOT NULL,
+					 		kategori TEXT NOT NULL,
+					 		maliyet REAL NOT NULL
+					    );
+					""");
 			Parent root = FXMLLoader.load(getClass().getResource("form1.fxml"));
 			primaryStage.setTitle("STOK TAKİP");
 			primaryStage.setScene(new Scene(root));
